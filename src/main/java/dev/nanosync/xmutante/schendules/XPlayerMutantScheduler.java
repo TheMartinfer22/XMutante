46792b90dc94e382f3da692e46b4be6adc7152f8
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-10-25 15:41:38 -0300

    ✔ Implementation of variable for timer

--- src/main/java/dev/nanosync/xmutante/schendules/XPlayerMutantScheduler.java
+++ src/main/java/dev/nanosync/xmutante/schendules/XPlayerMutantScheduler.java
@@ -5,9 +5,9 @@ import dev.nanosync.xmutante.entities.XPlayer;
 import org.bukkit.Bukkit;
 
 public class XPlayerMutantScheduler {
-    public XPlayerMutantScheduler(int time){
+    public XPlayerMutantScheduler(){
         Bukkit.getScheduler().scheduleSyncRepeatingTask(XMutante.getInstance(), () -> {
             new XPlayer().randomMutant();
-        }, 1, time);
+        }, 1, XMutante.getInstance().getConfig().getInt("TempoDeEscolhaMutanteMinutos") * 1200L);
     }
 }
