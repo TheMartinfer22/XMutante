215030946bb1eb9c10b57a443f7dde457a1bfadd
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-10-25 13:59:20 -0300

    ✔ Created a class for String's (BoilerPlate)

---
+++ src/main/java/dev/nanosync/xmutante/util/MessageUtil.java
@@ -0,0 +1,8 @@
+package dev.nanosync.xmutante.util;
+
+public class MessageUtil {
+    public static String prefix = "[XMutante] ";
+    public static String noPermissionCommand = prefix + "Você não têm permissão para executar esse comando.";
+    public static String youNotMutantCommand = prefix + "Você não é um mutante, então não pode executar esse comando";
+    public static String onlyPlayerCommand = prefix + "Somente jogadores podem executar esse comando.";
+}
