aca5701e6a27c94c7958dc8a09ffaf7ebb294018
Martin Hartfelder <66142507+TheMartinfer22@users.noreply.github.com>
2021-10-25 14:03:20 -0300

    ✔ Added methods to select, give a mutant player and clear then

--- src/main/java/dev/nanosync/xmutante/mutant/XPlayer.java
+++ src/main/java/dev/nanosync/xmutante/mutant/XPlayer.java
@@ -1,22 +1,66 @@
 package dev.nanosync.xmutante.mutant;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
+import dev.nanosync.xmutante.XMutante;
+import dev.nanosync.xmutante.util.MessageUtil;
+import org.bukkit.Bukkit;
+import org.bukkit.Instrument;
+import org.bukkit.Note;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
-public class XPlayer implements CommandExecutor {
-    private org.bukkit.entity.Player player;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+public class XPlayer implements Listener {
+    // Classe reponsável por efinir quem é o mutante
+    private Player player;
 
-        Player p = (Player) sender;
+    public Player getMutant(){
+        if (XMutante.getInstance().getConfig().getString("Mutante").equals("Nenhum")){
+            randomMutant();
+        }
+        return Bukkit.getPlayer(XMutante.getInstance().getConfig().getString("Mutante"));
+    }
 
-        this.player = p;
+    public void setMutant(Player player){
+        XMutante.getInstance().getConfig().set("Mutante", player.getName());
+        XMutante.getInstance().saveConfig();
+        this.player = player;
+    }
 
-        new XMenu(player).openMenu();
+    public void randomMutant(){
+        List<Player> players = new ArrayList<>(Bukkit.getOnlinePlayers());
+        Player player = players.get(new Random().nextInt(Bukkit.getOnlinePlayers().size()));
+        setMutant(player);
+        players.forEach(jogador -> {
+            jogador.sendTitle(MessageUtil.prefix, "O jogador " + player.getName() + " agora é mutante!", 1, 15, 1);
+            jogador.playNote(player.getPlayer().getLocation(), Instrument.BELL, Note.flat(1, Note.Tone.E));
+        });
+        player.sendMessage(MessageUtil.prefix + "Veja seus comandos em /mutante!");
+    }
 
-        return false;
+    public void clearMutant(){
+        XMutante.getInstance().getConfig().set("Mutante", "Nenhum");
+        XMutante.getInstance().saveConfig();
+    }
+
+    @EventHandler
+    public void onPlayerLeft(PlayerQuitEvent event) {
+        if (getMutant().getPlayer().equals(event.getPlayer())) {
+            XPowers xPowers = new XPowers(getMutant());
+            xPowers.disableAll();
+        }
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        if (getMutant().getPlayer().equals(event.getPlayer())) {
+            XPowers xPowers = new XPowers(getMutant());
+            xPowers.disableAll();
+        }
     }
 }
